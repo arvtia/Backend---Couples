b7e759b69ca5b8435acc5db148c05c3cfbc3864a
Arvtia <arvazanakuzhy@gmail.com>
2025-09-25 20:57:02 +0530

    fixed some issue caused app to crash

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
+// models/User.js
+const mongoose = require('mongoose');
+
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   name:  { type: String },
   password: { type: String, required: true },
   partnerCode: { type: String, unique: true },
-  coupleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Couple' },
-
-  // 🔐 Add these two fields
+   // 🔐 Add these two fields
   resetPasswordToken: { type: String },
-  resetPasswordExpires: { type: Date }
+  resetPasswordExpires: { type: Date },
+  coupleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Couple' }
 }, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
